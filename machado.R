8ab3bdc3d09fa9aa49f738d816b15083ee86f7f0
guiajf <guilhermeferreirajf@gmail.com>
2026-07-10 21:59:29 +0000

    Initial commit

--- machado.R
+++ machado.R
@@ -3,7 +3,31 @@ library(dplyr)
 listar_obras()
 listar_obras() %>% filter(titulo == "Junqueira Freire")
 
-library(usethis)
+# Instalar gitcreds se não tiver
+#install.packages("gitcreds")
 
-# Isso deve funcionar agora
+# Guardar o token
+gitcreds::gitcreds_set()
+# Cole o token quando pedir
+
+# Agora tente novamente
+usethis::use_github()
+
+usethis::use_pkgdown()
+
+usethis::use_github_action("check-standard")
+
+# Gerar o site localmente
+pkgdown::build_site()
+
+# Fazer deploy no GitHub Pages
+usethis::use_pkgdown_github_pages()
+
+
+# Adicionar os novos arquivos ao Git
 usethis::use_git()
+
+# Escolha a opção 1 (I agree) para commitar todos os arquivos
+
+# Enviar para o GitHub
+usethis::use_github()
